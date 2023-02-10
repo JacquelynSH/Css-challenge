15fabf956c2acaf484b0e4e7a9761bcf4891a210
JacquelynSH <elpatchaembroidery@gmail.com>
2023-02-10 11:40:14 -0800

    Add settimeout to allow delay from open class to cross class

--- day-two/main.js
+++ day-two/main.js
@@ -3,11 +3,15 @@ const menuButton = document.querySelector('.menu-container');
 let menuOpen = false;
 
 menuButton.addEventListener('click', () => {
-  if(!menuOpen) {
+  if (!menuOpen) {
     menuButton.classList.add('open');
     menuOpen = true
+    setTimeout(function () {
+      menuButton.classList.add('cross')
+    }, 500);
   } else {
     menuButton.classList.remove('open');
+    menuButton.classList.remove('cross');
     menuOpen = false;
   }
 });
